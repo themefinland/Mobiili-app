c88f91022990b731a182a468be7f24c904f32f57
themefinland <timi.honk@gmail.com>
2014-12-07 16:40:43 +0200

    Lisätty pop-up skripti

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+
+$("#closeLink, #resetLink").click( function () { popup('popUpDiv')});
+$("#resetLink").click( function () { clearLS()});
+
 //------------kääntymis funktio
 var angle = 0;				
 setInterval(function(){
@@ -52,8 +57,7 @@ animatethis($('#sormi'), 5000);
 
 animatethisUp($('#muna'), 1000);	
 
-						// tarkasta tämä funktio joka kerta kun sivu ladataan
-						
+
 		if(localStorage.clickcount < 20 ){
 		      //muuta elementin, jonka id="tonni", scr - attribuutti halutuksi
 			document.getElementById("muna").setAttribute("src", "assets/images/muna.png");
@@ -108,5 +112,95 @@ function clickCounter() {
 //Seuraavan funktion sisään voi määritellä kaikki ne asiat, jotka haluaa tapahtuvan, kun peli aloitetaan alusta.
 function clearLS(){
 	 localStorage.clear();
-	 document.getElementById("result").innerHTML = "Aloita peli n&aumlp&aumlytt&aumlm&aumlll&auml munaa!";
+	 document.getElementById("result").innerHTML = ("Aloita peli n&aumlp&aumlytt&aumlm&aumlll&auml munaa!");
 	 document.getElementById("muna").setAttribute("src", "assets/images/muna.png");
+	 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------- Pop-up funktio
+
+function toggle(div_id) {
+	var el = document.getElementById(div_id);
+	if ( el.style.display == 'none' ) {	el.style.display = 'block';}
+	else {el.style.display = 'none';}
+}
+function blanket_size(popUpDivVar) {
+	if (typeof window.innerWidth != 'undefined') {
+		viewportheight = window.innerHeight;
+	} else {
+		viewportheight = document.documentElement.clientHeight;
+	}
+	if ((viewportheight > document.body.parentNode.scrollHeight) && (viewportheight > document.body.parentNode.clientHeight)) {
+		blanket_height = viewportheight;
+	} else {
+		if (document.body.parentNode.clientHeight > document.body.parentNode.scrollHeight) {
+			blanket_height = document.body.parentNode.clientHeight;
+		} else {
+			blanket_height = document.body.parentNode.scrollHeight;
+		}
+	}
+	var blanket = document.getElementById('blanket');
+	blanket.style.height = blanket_height + 'px';
+	var popUpDiv = document.getElementById(popUpDivVar);
+	popUpDiv_height=blanket_height/2-200;//200 is half popup's height
+	popUpDiv.style.top = popUpDiv_height + 'px';
+}
+function window_pos(popUpDivVar) {
+	if (typeof window.innerWidth != 'undefined') {
+		viewportwidth = window.innerHeight;
+	} else {
+		viewportwidth = document.documentElement.clientHeight;
+	}
+	if ((viewportwidth > document.body.parentNode.scrollWidth) && (viewportwidth > document.body.parentNode.clientWidth)) {
+		window_width = viewportwidth;
+	} else {
+		if (document.body.parentNode.clientWidth > document.body.parentNode.scrollWidth) {
+			window_width = document.body.parentNode.clientWidth;
+		} else {
+			window_width = document.body.parentNode.scrollWidth;
+		}
+	}
+	var popUpDiv = document.getElementById(popUpDivVar);
+	window_width=window_width/2-200;//200 is half popup's width
+	popUpDiv.style.left = window_width + 'px';
+}
+function popup(windowname) {
+	blanket_size(windowname);
+	window_pos(windowname);
+	toggle('blanket');
+	toggle(windowname);		
+}
+
+
+//---------------Pop-up funktio päättyy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
