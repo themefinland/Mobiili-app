f33805d1da5e246edc26af32e59a3ebffafc42c7
EeroG <eero.grondahl@metropolia.fi>
2015-01-08 18:07:17 +0200

    Päivitetty scriptit lopulliseen muotoonsa

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,31 @@
+//Funktio joka hakee tarvittaessa fasclick.js -komponentin
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
+
+//Ääni funktio
+function play_single_sound() {
+		document.getElementById('audiotag1').play();
+	}
 
 $(document).ready(function(){
-
 document.getElementById("winRar").style.display = "none";
 document.getElementById("sormi").style.opacity = "0";
 $("#closeLink, #resetLink").click( function () { popup('popUpDiv')});
 $("#resetLink").click( function () { clearLS()});
 
-//------------kääntymis funktio
-/*var angle = 0;				
-setInterval(function(){
-      angle+=0.02;
-     $("#pilvet1").rotate(angle);
-},1000);
 
-setInterval(function(){
-      angle+=0.03;
-     $("#pilvet2").rotate(angle);
-},1000);
-*/
-//----------------käntymis funktio
 
 //--------------- Sormen ja munan liikkumis funktiot
 	function animatethis(targetElement, speed) {
-    $(targetElement).animate({ left: "+=15%"},
+    $(targetElement).animate({ left: "+=40%"},
     {
         duration: speed,
         complete: function ()
         {
-            targetElement.animate({ left: "-=15%" },
+            targetElement.animate({ left: "-=40%" },
             {
                 duration: speed,
                 complete: function ()
@@ -62,72 +60,78 @@ setInterval(function(){
 
 
 
-					// kutsutään tämä funktio sivua ladatessa jotta munan status tarkastetaan kun käyttäjä tulee takaisin sivulle
-					// nämä funktiot määräävät enimmäkseen mitä tapahtuu kun käyttäjä on klikannut tarpeeksi
-													setInterval(function(){
-													$('#stars').fadeIn(1000).delay(3000).fadeOut(3000);
-													$('#stars2').fadeIn(6000).delay(0000).fadeOut(1000);
+// kutsutään tämä funktio sivua ladatessa jotta munan status tarkastetaan kun käyttäjä tulee takaisin sivulle
+// nämä funktiot määräävät enimmäkseen mitä tapahtuu kun käyttäjä on klikannut tarpeeksi
+setInterval(function(){
+		$('#stars').fadeIn(1000).delay(3000).fadeOut(3000);
+		$('#stars2').fadeIn(6000).delay(0000).fadeOut(1000);
+}, 7000);
 
-													}, 7000);
-													animatethisUp($('#aurinko'), 3000);	
-													animatethisUp($('#kuu'), 4000);	
-													animatethisUp($('#pilvet1'), 1000);	
-													animatethisUp($('#pilvet2'), 2000);
-													animatethisUp($('#planet'), 5000);
-													animatethis($('#sormi'), 1000);	
-														setInterval(function () {
-																	  if(localStorage.clickcount >=20 && localStorage.clickcount <= 40 ){
-																		document.getElementById("muna").setAttribute("src", "assets/images/Muna2.png");
-																				 document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
-																			   localStorage.clickcount = Number(localStorage.clickcount)+1;
-																			   document.getElementById("sormi").style.opacity = "1";
-																			   document.getElementById("sormi").setAttribute("src", "assets/images/fingerOfDoom.png");
-																				}
-																				}, 2000);
-														setInterval(function () {
-																	  if(localStorage.clickcount >=40 && localStorage.clickcount <= 80  ){
-																		document.getElementById("muna").setAttribute("src", "assets/images/Muna3.png");
-																				 document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
-																				 localStorage.clickcount = Number(localStorage.clickcount)+2;
-																			   document.getElementById("sormi").setAttribute("src", "assets/images/fistOfDeath.png");
-																				document.getElementById("sormi").style.opacity = "1";																			   
-																				}
-																				}, 2000);	
+animatethisUp($('#aurinko'), 3000);	
+animatethisUp($('#kuu'), 4000);	
+animatethisUp($('#pilvet1'), 1000);	
+animatethisUp($('#pilvet2'), 2000);
+animatethisUp($('#planet'), 5000);
+animatethis($('#sormi'), 1000);	
 
-														setInterval(function () {
-																				  	if(localStorage.clickcount >=80 && localStorage.clickcount <= 150  ){
-																					 localStorage.clickcount = Number(localStorage.clickcount)+3;	
-																					$('#laserBeam').fadeIn(3000).delay(20).fadeOut(480);
-																				}
-																				}, 4500);
+setInterval(function () {
+		if(localStorage.clickcount >=20 && localStorage.clickcount <= 40 ){
+			document.getElementById("muna").setAttribute("src", "assets/images/Muna2.png");
+			document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
+			localStorage.clickcount = Number(localStorage.clickcount)+1;
+			document.getElementById("sormi").style.opacity = "1";
+			document.getElementById("sormi").setAttribute("src", "assets/images/fingerOfDoom.png");
+			}
+			}, 2000);
+			
+setInterval(function () {
+	if(localStorage.clickcount >=40 && localStorage.clickcount <= 80  ){
+		document.getElementById("muna").setAttribute("src", "assets/images/Muna3.png");
+		document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
+		localStorage.clickcount = Number(localStorage.clickcount)+2;
+		document.getElementById("sormi").setAttribute("src", "assets/images/fistOfDeath.png");
+		document.getElementById("sormi").style.opacity = "1";																			   
+	}
+}, 2000);	
 
-														setInterval(function () {
-																						if(localStorage.clickcount >=80 && localStorage.clickcount <= 150  ){
-																							document.getElementById("muna").setAttribute("src", "assets/images/Muna4.png");
-																							 document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
-																							 localStorage.clickcount = Number(localStorage.clickcount)+2;
-																							 document.getElementById("laser").style.display = "inline";
-																							 document.getElementById("laserBeam").style.display = "inline";
-																							  document.getElementById("sormi").setAttribute("src", "assets/images/fistOfDeath.png");
-																							document.getElementById("sormi").style.opacity = "1";			
-																							 }
-																							}, 2000);
+setInterval(function () {
+	if(localStorage.clickcount >=80 && localStorage.clickcount <= 150  ){
+		document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
+		localStorage.clickcount = Number(localStorage.clickcount)+3;	
+		$('#laserBeam').fadeIn(0).delay(0).fadeOut(2000);
+	}
+}, 4500);
+
+setInterval(function () {
+	if(localStorage.clickcount >=80 && localStorage.clickcount <= 150  ){
+		document.getElementById("muna").setAttribute("src", "assets/images/Muna4.png");
+		document.getElementById("result").innerHTML = localStorage.clickcount + " Clicks, keep going!";
+		localStorage.clickcount = Number(localStorage.clickcount)+2;
+		document.getElementById("laser").style.display = "inline";
+		document.getElementById("laserBeam").style.display = "inline";
+		document.getElementById("sormi").setAttribute("src", "assets/images/fistOfDeath.png");
+		document.getElementById("sormi").style.opacity = "1";			
+	}
+}, 2000);
 																							
-														if( localStorage.clickcount >= 150  ){
-																							document.getElementById("muna").setAttribute("src", "assets/images/Muna999.png");
-																							 document.getElementById("result").innerHTML = localStorage.clickcount + " You are winrar!!";
-																							 document.getElementById("laser").style.display = "inline";
-																							 document.getElementById("laserBeam").style.display = "none";
-																							document.getElementById("sormi").style.opacity = "0";
-																							$('#winRar').fadeIn(3000);							
-																							
-
-																								
-																							//katsotaan mitä käyttäjä saa
-																				
-																							 }
-																				
-
+if( localStorage.clickcount >= 150  ){
+	document.getElementById("muna").setAttribute("src", "assets/images/Muna999.png");
+	document.getElementById("result").innerHTML = localStorage.clickcount + " You are winrar!!";
+	document.getElementById("laser").style.display = "inline";
+	document.getElementById("laserBeam").style.display = "none";
+	document.getElementById("sormi").style.opacity = "0";
+	$('#winRar').fadeIn(3000);							
+	
+	//katsotaan mitä käyttäjä saa		
+ }
+ //munan animaatio ja ääni sitä klikatessa
+$( "#muna" ).click(function() {
+			$(this).animate({left:"+=3%"},150);
+			$(this).animate({left:"-=3%"},150);
+			$('#result').animate({top:"+=2%"},100);
+			$('#result').animate({top:"-=2%"},100);
+			play_single_sound();
+	});
 });
 
 
@@ -147,7 +151,6 @@ function clickCounter() {
 		//tästä eteenpäin määritellään klikkirajoja, joiden jälkeen määriteltyjä asioita tapahtuu
 		//Jos localstorageen kerääntynyt klikkimäärä on suurempi kuin 20
 		
-		
 		if(localStorage.clickcount == 20){
 		      //muuta elementin, jonka id="tonni", scr - attribuutti halutuksi
 			document.getElementById("muna").setAttribute("src", "assets/images/Muna2.png");
@@ -155,40 +158,35 @@ function clickCounter() {
 			document.getElementById("sormi").style.opacity = "1";
 		}
 		if(localStorage.clickcount == 40){
-		document.getElementById("muna").setAttribute("src", "assets/images/Muna3.png");
-		document.getElementById("sormi").setAttribute("src", "assets/images/fistOfDeath.png");
-		document.getElementById("sormi").style.opacity = "1";	
+			document.getElementById("muna").setAttribute("src", "assets/images/Muna3.png");
+			document.getElementById("sormi").setAttribute("src", "assets/images/fistOfDeath.png");
+			document.getElementById("sormi").style.opacity = "1";	
 				}
 		if(localStorage.clickcount == 80){
-		 document.getElementById("sormi").style.opacity = "1";	
-		 document.getElementById("muna").setAttribute("src", "assets/images/Muna4.png");
+		 	document.getElementById("sormi").style.opacity = "1";	
+		 	document.getElementById("muna").setAttribute("src", "assets/images/Muna4.png");
 		}
 		if(localStorage.clickcount > 150){
-		
-																							document.getElementById("muna").setAttribute("src", "assets/images/Muna999.png");
-																							 document.getElementById("result").innerHTML = localStorage.clickcount + " You are winrar!!";
-																							 document.getElementById("laser").style.display = "inline";
-																							 document.getElementById("laserBeam").style.display = "none";
-																							document.getElementById("sormi").style.opacity = "0";
-																							$('#winRar').fadeIn(3000);
+			document.getElementById("muna").setAttribute("src", "assets/images/Muna999.png");
+			document.getElementById("result").innerHTML = localStorage.clickcount + " You are winrar!!";
+			document.getElementById("laser").style.display = "inline";
+			document.getElementById("laserBeam").style.display = "none";
+			document.getElementById("sormi").style.opacity = "0";
+		$('#winRar').fadeIn(3000);
 																							
-																							
-																								var  randomi = Math.floor ((Math.random() * 10) +1);
-																									document.getElementById("prize").style.display = "inline";
-																									$("#prize").animate({minHeight:"30%", minWidth:"30%",paddingBottom:"40%"});
-																									if (randomi < 3){
-																								   document.getElementById("prize").setAttribute("src", "assets/images/fingerOfDoom.png");
-																								   
-																								   }
-																								  else if (randomi < 6 && randomi >= 3){
-																								   document.getElementById("prize").setAttribute("src", "assets/images/fistOfDeath.png");
-																								   }
-																								   else{
-																								   document.getElementById("prize").setAttribute("src", "assets/images/muna.png");
-																								   }
-																				   
-																								console.log(randomi);
+//Random palkinnon arpominen																							
+var  randomi = Math.floor ((Math.random() * 10) +1);
+	document.getElementById("prize").style.display = "inline";
+	$("#prize").animate({minHeight:"30%", minWidth:"30%",paddingBottom:"40%"});
+	if (randomi < 4){
+		document.getElementById("prize").setAttribute("src", "assets/images/prize3.png");
+		}else if(randomi < 7 && randomi >= 4){
+			document.getElementById("prize").setAttribute("src", "assets/images/prize2.png");
+		}else{
+			document.getElementById("prize").setAttribute("src", "assets/images/prize1.png");
 		}
+		console.log(randomi);
+	}
 }
 
 //Seuraava funktio tyhjentää local storagen, eli "tapit", poistaa kaikki "palkinnot" eli kuvat ja
@@ -216,3 +214,10 @@ function popup(windowname) {
 	toggle('blanket');
 	toggle(windowname);
 }
+
+
+
+
+
+
+
